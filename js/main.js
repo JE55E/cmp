858b3f4bfe1b18d5831e72c268651c74a5d401b1
je5c <jessethefirst@gmail.com>
2020-12-06 22:25:29 -0800

    new features and backup files

---
+++ js/main.js
@@ -0,0 +1,14 @@
+
+$(document).ready(function(){
+$('button').click(function() {
+    $('#enter').css({
+        'display': 'none',
+    });
+    $('#slogan').css({
+        'display': 'none',
+    });
+   $('#main').css({
+   			'filter': 'saturate(1)',
+   });
+});
+});
