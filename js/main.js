adbe0b3fe7d4b2340825d2f6883d6c997a3e3d2b
je5c <jessethefirst@gmail.com>
2020-12-07 23:09:57 -0800

    style

--- js/main.js
+++ js/main.js
@@ -1,14 +1,20 @@
-
 $(document).ready(function(){
-$('button').click(function() {
-    $('#enter').css({
-        'display': 'none',
-    });
-    $('#slogan').css({
-        'display': 'none',
-    });
-   $('#main').css({
-   			'filter': 'saturate(1)',
-   });
+	$('button').click(function() {
+		$('#enter').css({
+			'display': 'none',
+		});
+		$('#slogan').css({
+			'display': 'none',
+		});
+		$('#main').css({
+			'filter': 'saturate(1)',
+		});
+		$('#logo').css({
+			'display': 'inline-block',
+		});
+		$('#cmp').css({
+			'display': 'inline-block',
+		});
+	});
 });
-});
+
