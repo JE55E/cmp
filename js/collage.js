564bb3997e282fa70c498aabc3b52e12fa847d4b
je55e <jessethefirst@gmail.com>
2020-12-28 14:48:20 -0800

    -

---
+++ js/collage.js
@@ -0,0 +1,19 @@
+$(document).ready(function(){
+	$('button').click(function() {
+		$('#enter').css({
+			'display': 'none',
+		});
+		$('#slogan').css({
+			'display': 'none',
+		});
+		$('#main').css({
+			'filter': 'saturate(1)',
+		});
+		$('#logo').css({
+			'display': 'inline-block',
+		});
+		$('#cmp').css({
+			'display': 'inline-block',
+		});
+	});
+});
--- js/main.js
+++ js/main.js
@@ -1,20 +1,36 @@
+// SHOW & HIDE ELEMENTS
+
 $(document).ready(function(){
-	$('button').click(function() {
-		$('#enter').css({
-			'display': 'none',
-		});
-		$('#slogan').css({
-			'display': 'none',
-		});
-		$('#main').css({
-			'filter': 'saturate(1)',
-		});
-		$('#logo').css({
-			'display': 'inline-block',
-		});
-		$('#cmp').css({
-			'display': 'inline-block',
-		});
-	});
+  $('button').click(function() {
+    $('#slogan').css({
+      'display': 'none',
+    });
+    $('.tablink').css({
+      'mix-blend-mode': 'normal',
+    });
+  
 });
+});
+
+// MAIN MENU
+function openPage(pageName,elmnt,color) {
+  var i, tabcontent, tablinks;
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+  tablinks = document.getElementsByClassName("tablink");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].style.backgroundColor = "";
+  }
+  document.getElementById(pageName).style.display = "block";
+  elmnt.style.backgroundColor = color;
+}
+
+// p5
+// createCanvas(windowWidth, windowHeight, WEBGL);
+// background('rgba(0,255,0, 0)');
+
+
+
 
